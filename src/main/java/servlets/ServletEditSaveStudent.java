a5dd3f3913596ec0ff0305655fc76df03caf741d
X1 Carbon <mahiich@mail.ru>
2021-02-13 16:24:16 +0500

    2 modifying editsave function

--- src/main/java/servlets/ServletEditSaveStudent.java
+++ src/main/java/servlets/ServletEditSaveStudent.java
@@ -10,7 +10,7 @@ public class ServletEditSaveStudent extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-//        String s=request.getParameter("name");
+        String id= request.getParameter("id");
 
         PrintWriter printWriter = response.getWriter();
         Connection connection = null;
@@ -19,12 +19,108 @@ public class ServletEditSaveStudent extends HttpServlet {
             String dburl = "jdbc:postgresql://localhost:5432/studentservlet";
             connection = DriverManager.getConnection(dburl, "postgres", "admin");
 
-            String sql = "select * from students";//todo
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement(sql);
+            if (id!=null) {
+                String sql = "select * from students where id=?";//todo
+                PreparedStatement preparedStatement =
+                        connection.prepareStatement(sql);
+                preparedStatement.setShort(1, Short.parseShort(id));
+                ResultSet resultSet = preparedStatement.executeQuery();
 
-            ResultSet resultSet = preparedStatement.executeQuery();
 
+                printWriter.println("<html>");
+
+                printWriter.println("    <div id=\"formdiv\">\n" +
+                        "        <form method=\"POST\">\n" +
+
+                        "            <label for=\"fname\">Name:</label><br>\n" +
+                        "            <input type=\"text\" id=\"fname\" required name=\"name\" value=\""+resultSet.getString("name")+"\"><br>\n" +
+                        "\n" +
+                        "            <label for=\"femail\">Email:</label><br>\n" +
+                        "            <input type=\"email\" id=\"femail\" required name=\"email\" value=\""+resultSet.getString("email")+"\"><br>\n" +
+                        "\n" +
+                        "            <label for=\"fcourse\">Course:</label><br>\n" +
+                        "            <input type=\"number\" id=\"fcourse\" required name=\"course\" value=\""+resultSet.getShort("course")+"\"><br>\n" +
+                        "\n" +
+                        "            <label for=\"fage\">Age:</label><br>\n" +
+                        "            <input type=\"number\" id=\"fage\" required name=\"age\" value=\""+resultSet.getShort("age")+"\"><br>\n" +
+                        "\n" +
+                        "            <input id=\"submitbutton\" type=\"submit\" value=\"Save\">\n" +
+                        "        </form>\n" +
+                        "\n" +
+                        "    </div>\n" +
+                        "\n");
+                printWriter.println("<style>\n" +
+                        "        label {\n" +
+                        "            color: red;\n" +
+                        "            width: 30px;\n" +
+                        "        }\n" +
+                        "        \n" +
+                        "        #submitbutton {\n" +
+                        "            margin: 10px 30px auto;\n" +
+                        "            width: 100px;\n" +
+                        "            color: rgb(219, 81, 17);\n" +
+                        "            background-color: cornsilk;\n" +
+                        "        }\n" +
+                        "        \n" +
+                        "        #formdiv {\n" +
+                        "            padding: 2rem;\n" +
+                        "            width: 172px;\n" +
+                        "            border: 3px solid salmon;\n" +
+                        "            margin: 14% 42% auto;\n" +
+                        "        }\n" +
+                        "    </style>");
+
+                printWriter.println("</html>");
+
+
+
+            }
+            else if (id==null){
+                printWriter.println("<html>");
+
+                printWriter.println("    <div id=\"formdiv\">\n" +
+                        "        <form method=\"POST\">\n" +
+
+                        "            <label for=\"fname\">Name:</label><br>\n" +
+                        "            <input type=\"text\" id=\"fname\" required name=\"name\" value=\"\"><br>\n" +
+                        "\n" +
+                        "            <label for=\"femail\">Email:</label><br>\n" +
+                        "            <input type=\"email\" id=\"femail\" required name=\"email\" value=\"\"><br>\n" +
+                        "\n" +
+                        "            <label for=\"fcourse\">Course:</label><br>\n" +
+                        "            <input type=\"number\" id=\"fcourse\" required name=\"course\" value=\"\"><br>\n" +
+                        "\n" +
+                        "            <label for=\"fage\">Age:</label><br>\n" +
+                        "            <input type=\"number\" id=\"fage\" required name=\"age\" value=\"\"><br>\n" +
+                        "\n" +
+                        "            <input id=\"submitbutton\" type=\"submit\" value=\"Save\">\n" +
+                        "        </form>\n" +
+                        "\n" +
+                        "    </div>\n" +
+                        "\n");
+                    printWriter.println("<style>\n" +
+                            "        label {\n" +
+                            "            color: red;\n" +
+                            "            width: 30px;\n" +
+                            "        }\n" +
+                            "        \n" +
+                            "        #submitbutton {\n" +
+                            "            margin: 10px 30px auto;\n" +
+                            "            width: 100px;\n" +
+                            "            color: rgb(219, 81, 17);\n" +
+                            "            background-color: cornsilk;\n" +
+                            "        }\n" +
+                            "        \n" +
+                            "        #formdiv {\n" +
+                            "            padding: 2rem;\n" +
+                            "            width: 172px;\n" +
+                            "            border: 3px solid salmon;\n" +
+                            "            margin: 14% 42% auto;\n" +
+                            "        }\n" +
+                            "    </style>");
+
+                printWriter.println("</html>");
+            }
 
 
 
