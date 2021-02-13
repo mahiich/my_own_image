668ee21224e2d6a925d9cf7754fa1d88a3991ce3
X1 Carbon <mahiich@mail.ru>
2021-02-13 09:52:36 +0500

    buttons decoration

--- src/main/java/servlets/ServletGetAddStudent.java
+++ src/main/java/servlets/ServletGetAddStudent.java
@@ -28,10 +28,11 @@ public class ServletGetAddStudent extends HttpServlet {
             int tablecounter=1;
             printWriter.println("<html>");
             printWriter.println("<h1 style=\"text-align:center\";>Students list</h1>");
-            printWriter.println("<a href=\"/editSave\" ></a>");
-            printWriter.println("<table border:2px>");
+            printWriter.println("<a href=\"/editSave\" id=\"addbutton\" >Add student</a>");
+            printWriter.println("<table style=\"border:1px solid black\" id=\"table\">");
+            printWriter.println("<caption>This is table caption</caption>");
             printWriter.println(
-                    "  <tr>\n" +
+                    "  <tr style=\"border:1px solid black\">\n" +
                     "    <th>#</th>\n" +
                     "    <th>id</th>\n" +
                     "    <th>name</th>\n" +
@@ -43,7 +44,7 @@ public class ServletGetAddStudent extends HttpServlet {
                     "  </tr>");
 
             while (resultSet.next()){
-                printWriter.println("<tr>");
+                printWriter.println("<tr style=\"border:1px solid black\">");
 
                 printWriter.println("<td>"+tablecounter+"</td>");
                 printWriter.println("<td>"+resultSet.getShort("id")+"</td>");
@@ -51,8 +52,8 @@ public class ServletGetAddStudent extends HttpServlet {
                 printWriter.println("<td>"+resultSet.getString("email")+"</td>");
                 printWriter.println("<td>"+resultSet.getString("course")+"</td>");
                 printWriter.println("<td>"+resultSet.getString("age")+"</td>");
-                printWriter.println("<td><a href=\"/editSave?id="+resultSet.getShort("id")+"\">Edit</a></td>");
-                printWriter.println("<td><a href=\"/delete?id="+resultSet.getShort("id")+"\">Delete</a></td>");
+                printWriter.println("<td><a id=\"editbutton\" href=\"/editSave?id="+resultSet.getShort("id")+"\">Edit</a></td>");
+                printWriter.println("<td><a id=\"deletebutton\"href=\"/delete?id="+resultSet.getShort("id")+"\">Delete</a></td>");
 
                 tablecounter++;
                 printWriter.println("</tr>");
@@ -67,7 +68,55 @@ public class ServletGetAddStudent extends HttpServlet {
 
 
 
+            printWriter.println("<style>" +        //************  CSS styles
+                    "#table tr:nth-child(even) {\n" +
+                    "  background-color: #eee;\n" +
+                    "}\n" +
+                    "#table tr:nth-child(odd) {\n" +
+                    "  background-color: #fff;\n" +
+                    "}\n" +
+                    "#table th {\n" +
+                    "  color: white;\n" +
+                    "  background-color: black;\n" +
+                    "  background-color: black;\n" +
+                    "}" +
 
+                    "#addbutton:link {\n" +
+                    "  color: white;\n" +
+                    "  text-color:yellow;\n" +
+                    "  background-color: green;\n" +
+                    "  text-decoration: none;\n" +
+                    "  border:1px solid red;\n" +
+                    "}\n" +
+                    "#addbutton:visited {\n" +
+                    "  color: pink;\n" +
+                    "  background-color: transparent;\n" +
+                    "  text-decoration: none;\n" +
+                    "}\n" +
+                    "#addbutton:hover {\n" +
+                    "  color: red;\n" +
+                    "  background-color: transparent;\n" +
+                    "  text-decoration: underline;\n" +
+                    "}\n" +
+                    "#addbutton:active {\n" +
+                    "  color: yellow;\n" +
+                    "  background-color: transparent;\n" +
+                    "  text-decoration: underline;\n" +
+                    "}" +
+
+                    "#editbutton{" +
+                    "color:black;\n" +
+                    "background-color:yellow;\n" +
+                    "text-decoration: underline;\n" +
+                    "}"+
+
+                    "#deletebutton{" +
+                    "color:white;\n" +
+                    "background-color:red;\n" +
+                    "text-decoration: underline;\n" +
+                    "}"+
+
+                    "</style>");
 
             printWriter.println("<html>");
 
